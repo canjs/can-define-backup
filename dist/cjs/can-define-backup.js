76b6c4bcf2678d1016a284161083055390a277c7
Justin Meyer <justinbmeyer@gmail.com>
2018-04-30 17:18:08 -0500

    2.0.0-pre.1

---
+++ dist/cjs/can-define-backup.js
@@ -0,0 +1,71 @@
+/*can-define-backup@2.0.0-pre.0#can-define-backup*/
+var assign = require('can-assign');
+var canReflect = require('can-reflect');
+var SimpleObservable = require('can-simple-observable');
+var diffDeep = require('can-diff/deep/deep');
+var diffMap = require('can-diff/map/map');
+var flatProps = function (a, cur) {
+    var obj = {};
+    for (var prop in a) {
+        if (typeof a[prop] !== 'object' || a[prop] === null || a[prop] instanceof Date) {
+            obj[prop] = a[prop];
+        } else {
+            obj[prop] = cur[prop];
+        }
+    }
+    return obj;
+};
+var observables = new WeakMap();
+function getBackup(map) {
+    var obs = observables.get(map);
+    if (!obs) {
+        obs = new SimpleObservable();
+        observables.set(map, obs);
+    }
+    return obs;
+}
+function defineBackup(Map) {
+    assign(Map.prototype, {
+        backup: function () {
+            var store = getBackup(this);
+            canReflect.setValue(store, this.serialize());
+            return this;
+        },
+        isDirty: function (checkAssociations) {
+            var store = getBackup(this);
+            var backupStore = canReflect.getValue(store);
+            if (!backupStore) {
+                return false;
+            }
+            var currentValue = this.serialize();
+            var patches;
+            if (!!checkAssociations) {
+                patches = diffDeep(currentValue, backupStore);
+            } else {
+                patches = diffMap(currentValue, backupStore).filter(function (patch) {
+                    if (patch.type !== 'set') {
+                        return true;
+                    } else {
+                        var curVal = currentValue[patch.key], backupVal = backupStore[patch.key];
+                        var twoObjectsCompared = curVal && backupVal && typeof curVal === 'object' && typeof backupVal === 'object';
+                        return !twoObjectsCompared;
+                    }
+                });
+            }
+            return patches.length;
+        },
+        restore: function (restoreAssociations) {
+            var store = getBackup(this);
+            var curVal = canReflect.getValue(store);
+            var props = restoreAssociations ? curVal : flatProps(curVal, this);
+            if (this.isDirty(restoreAssociations)) {
+                for (var prop in props) {
+                    this[prop] = props[prop];
+                }
+            }
+            return this;
+        }
+    });
+    return;
+}
+module.exports = exports = defineBackup;
